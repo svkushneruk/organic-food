a075e60de2cd62a23ba965f1c41c0dab1d5e1722
Serhii Kushneruk <svkushneruk@ukr.net>
2022-03-16 21:03:10 +0200

    Додає оформлення footer

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,6 +1,8 @@
 var swiper = new Swiper(".swiper", {
-                pagination: {
-                    el: ".swiper-pagination",
-                    clickable: true,
-                },
-            });
+                navigation: {
+                    nextEl: '.button-next',
+                    prevEl: '.button-prev',
+    },
+    autoplay: {
+   delay: 5000,
+    },       });
